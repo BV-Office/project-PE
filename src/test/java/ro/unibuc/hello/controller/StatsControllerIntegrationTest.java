7023e06d8d159f5fc53396bfc8f8634b26d8e6c6
Victor-Gabriel Iacob <119161177+Victor2626@users.noreply.github.com>
2025-04-14 20:25:38 +0000

    Configure jenkins lab 7

--- src/test/java/ro/unibuc/hello/controller/StatsControllerIntegrationTest.java
+++ src/test/java/ro/unibuc/hello/controller/StatsControllerIntegrationTest.java
@@ -77,7 +77,7 @@ public class StatsControllerIntegrationTest {
 
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry registry) {
-        final String MONGO_URL = "mongodb://localhost:";
+        final String MONGO_URL = "mongodb://host.docker.internal:";
         final String PORT = String.valueOf(mongoDBContainer.getMappedPort(27017));
 
         registry.add("mongodb.connection.url", () -> MONGO_URL + PORT);
