71403d1ddccb23e19691f76be41beb5e3f1f3cc5
M-Podi <114012309+M-Podi@users.noreply.github.com>
2025-05-05 21:29:53 +0000

    Added MetricsConfig

--- src/main/java/ro/unibuc/hello/controller/BidController.java
+++ src/main/java/ro/unibuc/hello/controller/BidController.java
@@ -1,5 +1,7 @@
 package ro.unibuc.hello.controller;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Timer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,61 +20,141 @@ public class BidController {
     @Autowired
     private BidService bidService;
 
+    @Autowired
+    private Counter httpRequestsTotal;
+
+    @Autowired
+    private Counter successfulOperations;
+
+    @Autowired
+    private Counter failedOperations;
+
+    @Autowired
+    private Timer operationLatency;
+
+    @Autowired
+    private Counter activeUsers;
+
     @GetMapping
     public ResponseEntity<List<Bid>> getAllBids() {
-        List<Bid> bids = bidService.getAllBids();
-        return new ResponseEntity<>(bids, HttpStatus.OK);
+        httpRequestsTotal.increment();
+        Timer.Sample sample = Timer.start();
+        try {
+            List<Bid> bids = bidService.getAllBids();
+            successfulOperations.increment();
+            sample.stop(operationLatency);
+            return new ResponseEntity<>(bids, HttpStatus.OK);
+        } catch (Exception e) {
+            failedOperations.increment();
+            sample.stop(operationLatency);
+            throw e;
+        }
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Bid> getBidById(@PathVariable String id) {
+        httpRequestsTotal.increment();
+        Timer.Sample sample = Timer.start();
         try {
             Bid bid = bidService.getBidById(id);
+            successfulOperations.increment();
+            sample.stop(operationLatency);
             return new ResponseEntity<>(bid, HttpStatus.OK);
         } catch (EntityNotFoundException e) {
+            failedOperations.increment();
+            sample.stop(operationLatency);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @GetMapping("/item/{itemId}")
     public ResponseEntity<List<Bid>> getBidsByItem(@PathVariable String itemId) {
-        List<Bid> bids = bidService.getBidsByItem(itemId);
-        return new ResponseEntity<>(bids, HttpStatus.OK);
+        httpRequestsTotal.increment();
+        Timer.Sample sample = Timer.start();
+        try {
+            List<Bid> bids = bidService.getBidsByItem(itemId);
+            successfulOperations.increment();
+            sample.stop(operationLatency);
+            return new ResponseEntity<>(bids, HttpStatus.OK);
+        } catch (Exception e) {
+            failedOperations.increment();
+            sample.stop(operationLatency);
+            throw e;
+        }
     }
 
     @GetMapping("/bidder/{bidderName}")
     public ResponseEntity<List<Bid>> getBidsByBidder(@PathVariable String bidderName) {
-        List<Bid> bids = bidService.getBidsByBidder(bidderName);
-        return new ResponseEntity<>(bids, HttpStatus.OK);
+        httpRequestsTotal.increment();
+        Timer.Sample sample = Timer.start();
+        try {
+            List<Bid> bids = bidService.getBidsByBidder(bidderName);
+            successfulOperations.increment();
+            activeUsers.increment();
+            sample.stop(operationLatency);
+            return new ResponseEntity<>(bids, HttpStatus.OK);
+        } catch (Exception e) {
+            failedOperations.increment();
+            sample.stop(operationLatency);
+            throw e;
+        }
     }
 
     @PostMapping
     public ResponseEntity<?> placeBid(@RequestBody Bid bid) {
+        httpRequestsTotal.increment();
+        Timer.Sample sample = Timer.start();
         try {
             Bid placedBid = bidService.placeBid(bid);
+            successfulOperations.increment();
+            activeUsers.increment();
+            sample.stop(operationLatency);
             return new ResponseEntity<>(placedBid, HttpStatus.CREATED);
         } catch (BidException e) {
+            failedOperations.increment();
+            sample.stop(operationLatency);
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         } catch (EntityNotFoundException e) {
+            failedOperations.increment();
+            sample.stop(operationLatency);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } catch (IllegalArgumentException e) {
+            failedOperations.increment();
+            sample.stop(operationLatency);
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteBid(@PathVariable String id) {
+        httpRequestsTotal.increment();
+        Timer.Sample sample = Timer.start();
         try {
             bidService.deleteBid(id);
+            successfulOperations.increment();
+            sample.stop(operationLatency);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (EntityNotFoundException e) {
+            failedOperations.increment();
+            sample.stop(operationLatency);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @GetMapping("/bidder-email/{email}")
     public ResponseEntity<List<Bid>> getBidsByEmail(@PathVariable String email) {
-        List<Bid> bids = bidService.getBidsByEmail(email);
-        return new ResponseEntity<>(bids, HttpStatus.OK);
+        httpRequestsTotal.increment();
+        Timer.Sample sample = Timer.start();
+        try {
+            List<Bid> bids = bidService.getBidsByEmail(email);
+            successfulOperations.increment();
+            activeUsers.increment();
+            sample.stop(operationLatency);
+            return new ResponseEntity<>(bids, HttpStatus.OK);
+        } catch (Exception e) {
+            failedOperations.increment();
+            sample.stop(operationLatency);
+            throw e;
+        }
     }
 }
--- src/main/java/ro/unibuc/hello/controller/ItemController.java
+++ src/main/java/ro/unibuc/hello/controller/ItemController.java
@@ -1,5 +1,7 @@
 package ro.unibuc.hello.controller;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Timer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,68 +19,120 @@ public class ItemController {
     @Autowired
     private ItemService itemService;
 
+    @Autowired
+    private Counter httpRequestsTotal;
+
+    @Autowired
+    private Counter successfulOperations;
+
+    @Autowired
+    private Counter failedOperations;
+
+    @Autowired
+    private Timer operationLatency;
+
     @GetMapping
     public ResponseEntity<List<Item>> getAllItems(
             @RequestParam(required = false, defaultValue = "false") boolean activeOnly) {
-
-        List<Item> items;
-        if (activeOnly) {
-            items = itemService.getActiveItems();
-        } else {
-            items = itemService.getAllItems();
+        httpRequestsTotal.increment();
+        Timer.Sample sample = Timer.start();
+        try {
+            List<Item> items;
+            if (activeOnly) {
+                items = itemService.getActiveItems();
+            } else {
+                items = itemService.getAllItems();
+            }
+            successfulOperations.increment();
+            sample.stop(operationLatency);
+            return new ResponseEntity<>(items, HttpStatus.OK);
+        } catch (Exception e) {
+            failedOperations.increment();
+            sample.stop(operationLatency);
+            throw e;
         }
-
-        return new ResponseEntity<>(items, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Item> getItemById(@PathVariable String id) {
+        httpRequestsTotal.increment();
+        Timer.Sample sample = Timer.start();
         try {
             Item item = itemService.getItemById(id);
+            successfulOperations.increment();
+            sample.stop(operationLatency);
             return new ResponseEntity<>(item, HttpStatus.OK);
         } catch (EntityNotFoundException e) {
+            failedOperations.increment();
+            sample.stop(operationLatency);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @GetMapping("/search")
     public ResponseEntity<Item> searchItemByName(@RequestParam String name) {
+        httpRequestsTotal.increment();
+        Timer.Sample sample = Timer.start();
         try {
             Item item = itemService.searchItemByName(name);
+            successfulOperations.increment();
+            sample.stop(operationLatency);
             return new ResponseEntity<>(item, HttpStatus.OK);
         } catch (EntityNotFoundException e) {
+            failedOperations.increment();
+            sample.stop(operationLatency);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @PostMapping
     public ResponseEntity<Item> createItem(@RequestBody Item item) {
+        httpRequestsTotal.increment();
+        Timer.Sample sample = Timer.start();
         try {
             Item createdItem = itemService.createItem(item);
+            successfulOperations.increment();
+            sample.stop(operationLatency);
             return new ResponseEntity<>(createdItem, HttpStatus.CREATED);
         } catch (IllegalArgumentException e) {
+            failedOperations.increment();
+            sample.stop(operationLatency);
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Item> updateItem(@PathVariable String id, @RequestBody Item item) {
+        httpRequestsTotal.increment();
+        Timer.Sample sample = Timer.start();
         try {
             Item updatedItem = itemService.updateItem(id, item);
+            successfulOperations.increment();
+            sample.stop(operationLatency);
             return new ResponseEntity<>(updatedItem, HttpStatus.OK);
         } catch (EntityNotFoundException e) {
+            failedOperations.increment();
+            sample.stop(operationLatency);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } catch (IllegalArgumentException e) {
+            failedOperations.increment();
+            sample.stop(operationLatency);
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteItem(@PathVariable String id) {
+        httpRequestsTotal.increment();
+        Timer.Sample sample = Timer.start();
         try {
             itemService.deleteItem(id);
+            successfulOperations.increment();
+            sample.stop(operationLatency);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (EntityNotFoundException e) {
+            failedOperations.increment();
+            sample.stop(operationLatency);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
